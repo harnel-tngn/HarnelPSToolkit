5ac068688f0a382d6dec58666a9ce2d1debb30f9
Harnel <harnel.tngn@gmail.com>
2026-03-03 21:38:05 +0900

    feat: Added some algorithms

---
+++ src/HarnelPSToolkit.Library/SegTree/IAbelianGroupSegTree.cs
@@ -0,0 +1,10 @@
+﻿namespace HarnelPSToolkit.Library.SegTree;
+
+[IncludeIfReferenced]
+public interface IAbelianGroupSegTreeOperator<TElement, TUpdate, TDiff>
+{
+    TElement Identity();
+    TDiff CreateDiff(TElement element, TUpdate val);
+    TElement ApplyDiff(TElement element, TDiff diff);
+    TElement Merge(TElement l, TElement r);
+}
